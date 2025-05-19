13ba1fded3331bf1c94df16a0e95d84f1538b598
Jose De La Cruz <jose.delcruz0490@icloud.com>
2025-05-19 01:07:05 -0400

    restrict game to draw at 60 FPS

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
 from constants import *
 
-def game_loop(screen):
+
+      
+    
+
+def main():
+  pygame.init()
+  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
           return
       screen.fill('black')
       pygame.display.flip()
-    
+      dt = clock.tick(60) / 1000
 
-def main():
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  game_loop(screen)
 
 if __name__ == "__main__":
   main()
